e45d1733b8659862b2305d056cd61a5626ad3807
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-10-10 15:29:15 +0530

    feat: add code for Lecture 153: Autowire

--- java-based-configuration/src/main/java/com/maarcus/Main.java
+++ java-based-configuration/src/main/java/com/maarcus/Main.java
@@ -1,6 +1,7 @@
 package com.maarcus;
 
 import com.maarcus.config.AppConfig;
+import com.maarcus.model.Alien;
 import com.maarcus.model.Desktop;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -8,11 +9,17 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
 	public static void main(String[] args) {
 		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+		Alien alien = context.getBean(Alien.class);
+
+		alien.setAge(12);
+		System.out.println(alien.getAge());
+		alien.code();
 		
-		Desktop desktop1 = context.getBean(Desktop.class);
-		Desktop desktop2 = context.getBean(Desktop.class);
+		// Desktop desktop1 = context.getBean(Desktop.class);
+		// Desktop desktop2 = context.getBean(Desktop.class);
 		
-		desktop1.compile();
-		desktop2.compile();
+		// desktop1.compile();
+		// desktop2.compile();
 	}
 }
--- java-based-configuration/src/main/java/com/maarcus/config/AppConfig.java
+++ java-based-configuration/src/main/java/com/maarcus/config/AppConfig.java
@@ -1,15 +1,26 @@
 package com.maarcus.config;
 
+import com.maarcus.model.Alien;
+import com.maarcus.model.Computer;
 import com.maarcus.model.Desktop;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 	
-	@Bean(name = "desktop1")
-	@Scope(value = "prototype")
+	@Bean
+	public Alien alien(Computer computer) {
+		Alien alien = new Alien();
+		alien.setAge(25);
+		alien.setComputer(computer);
+		return alien;
+	}
+	
+	@Bean
+	// @Scope(value = "prototype")
 	// Can also do this -> @Bean(name = { "desktop1", "desktop2", ... })
 	
 	public Desktop desktop() {
