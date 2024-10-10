5c896e56eb62038898e77df5cb7b015960ae103b
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-10-10 15:37:19 +0530

    feat: add code for Lecture 154 Primary and Qualifier

--- java-based-configuration/src/main/java/com/maarcus/config/AppConfig.java
+++ java-based-configuration/src/main/java/com/maarcus/config/AppConfig.java
@@ -4,15 +4,18 @@ import com.maarcus.model.Alien;
 import com.maarcus.model.Computer;
 import com.maarcus.model.Desktop;
 
+import com.maarcus.model.Laptop;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class AppConfig {
 	
 	@Bean
-	public Alien alien(Computer computer) {
+	public Alien alien(Computer computer) { // @Qualifier("desktop")
 		Alien alien = new Alien();
 		alien.setAge(25);
 		alien.setComputer(computer);
@@ -25,5 +28,11 @@ public class AppConfig {
 	
 	public Desktop desktop() {
 		return new Desktop();
+	}
+	
+	@Bean
+	@Primary
+	public Laptop laptop() {
+		return new Laptop();
 	}
 }
---
+++ java-based-configuration/src/main/java/com/maarcus/model/Laptop.java
@@ -0,0 +1,7 @@
+package com.maarcus.model;
+
+public class Laptop implements Computer {
+	public void compile() {
+		System.out.println("Compiling with laptop...");
+	}
+}
