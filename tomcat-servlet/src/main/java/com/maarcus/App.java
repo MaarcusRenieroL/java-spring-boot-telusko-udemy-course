410f341b554e411a8b8a9da788708b598cb99b6e
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-10-15 17:58:34 +0530

    feat: add code for lecture 174: create a servlet project

---
+++ tomcat-servlet/src/main/java/com/maarcus/App.java
@@ -0,0 +1,11 @@
+package com.maarcus;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+}
