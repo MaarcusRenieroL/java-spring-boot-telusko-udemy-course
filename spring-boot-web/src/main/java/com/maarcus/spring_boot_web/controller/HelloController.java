1d7a6385c1370bff286ae197006b3847241bd99d
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-10-25 15:03:11 +0530

    feat: add code for lecure 182: request mapping

--- spring-boot-web/src/main/java/com/maarcus/spring_boot_web/controller/HelloController.java
+++ spring-boot-web/src/main/java/com/maarcus/spring_boot_web/controller/HelloController.java
@@ -1,10 +1,12 @@
 package com.maarcus.spring_boot_web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
 
+  @RequestMapping("/")
   public String hello() {
     return "index.jsp";
   }
