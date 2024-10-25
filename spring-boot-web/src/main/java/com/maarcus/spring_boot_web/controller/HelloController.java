7df5dbc0c7f270c46615082bf27389570ee98c5c
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-10-25 16:48:12 +0530

    feat: add code for lecture 186: request param

--- spring-boot-web/src/main/java/com/maarcus/spring_boot_web/controller/HelloController.java
+++ spring-boot-web/src/main/java/com/maarcus/spring_boot_web/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.maarcus.spring_boot_web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -14,10 +15,26 @@ public class HelloController {
     return "index.jsp";
   }
 
-  @RequestMapping("/add")
-  public String add(HttpServletRequest request, HttpSession session) {
+  // way 1
+  // @RequestMapping("/add")
+  // public String add(HttpServletRequest request, HttpSession session) {
 
-    session.setAttribute("result", Integer.parseInt(request.getParameter("num1")) + Integer.parseInt(request.getParameter("num2")));
+  //   session.setAttribute("result", Integer.parseInt(request.getParameter("num1")) + Integer.parseInt(request.getParameter("num2")));
+  //   return "result.jsp";
+  // }
+
+  // way 2
+  // @RequestMapping("/add")
+  // public String add(int num1, int num2, HttpSession session) {
+  //   session.setAttribute("result", num1 + num2);
+  //   return "result.jsp";
+  // }
+
+  // way 3
+  @RequestMapping("/add")
+  public String add(@RequestParam int num1, @RequestParam int num2, HttpSession session) {
+    session.setAttribute("result", num1 + num2);
     return "result.jsp";
   }
+
 }
