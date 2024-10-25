6bebe30a80609d0b85d15ea8c260e9b853d80e51
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-10-25 15:22:57 +0530

    feat: add code for lecture 184: accepting data the servlet way

--- spring-boot-web/src/main/java/com/maarcus/spring_boot_web/controller/HelloController.java
+++ spring-boot-web/src/main/java/com/maarcus/spring_boot_web/controller/HelloController.java
@@ -3,11 +3,20 @@ package com.maarcus.spring_boot_web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @Controller
 public class HelloController {
 
   @RequestMapping("/")
   public String hello() {
     return "index.jsp";
+  }
+
+  @RequestMapping("/add")
+  public String add(HttpServletRequest request) {
+
+    System.out.println(Integer.parseInt(request.getParameter("num1")) + Integer.parseInt(request.getParameter("num2")));
+    return "result.jsp";
   }
 }
